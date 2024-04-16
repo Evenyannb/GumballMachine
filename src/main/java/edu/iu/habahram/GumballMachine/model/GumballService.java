cd87372f12fbbb039da8de9b1f7763c6cc38244d
yanlian <yanlian@iu.edu>
2024-04-16 15:42:54 -0400

    GROUP10

---
+++ src/main/java/edu/iu/habahram/GumballMachine/model/GumballService.java
@@ -0,0 +1,76 @@
+package edu.iu.habahram.GumballMachine.model;
+
+import edu.iu.habahram.GumballMachine.repository.IGumballRepository;
+import edu.iu.habahram.GumballMachine.service.IGumballService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class GumballService implements IGumballService {
+
+    IGumballRepository gumballRepository;
+
+    public GumballService(IGumballRepository gumballRepository) {
+        this.gumballRepository = gumballRepository;
+    }
+
+    private TransitionResult transit(String id, Transition action) throws IOException {
+        GumballMachineRecord record = gumballRepository.findById(id);
+        IGumballMachine machine = new GumballMachine2(record.getId(), record.getState(), record.getCount());
+        TransitionResult result = runTheMachine(machine, action);
+        if(result.succeeded()) {
+            record.setState(result.stateAfter());
+            record.setCount(result.countAfter());
+            save(record);
+        }
+        return result;
+    }
+
+    private TransitionResult runTheMachine(IGumballMachine machine, Transition action) {
+        switch (action) {
+            case INSERT_QUARTER -> {
+                return machine.insertQuarter();
+            }
+            case EJECT_QUARTER -> {
+                return machine.ejectQuarter();
+            }
+            case TURN_CRANK -> {
+                return machine.turnCrank();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public TransitionResult insertQuarter(String id) throws IOException {
+        return transit(id, Transition.INSERT_QUARTER);
+    }
+
+    @Override
+    public TransitionResult ejectQuarter(String id) throws IOException {
+        return transit(id, Transition.EJECT_QUARTER);
+    }
+
+    @Override
+    public TransitionResult turnCrank(String id) throws IOException {
+        return transit(id, Transition.TURN_CRANK);
+    }
+
+
+
+    @Override
+    public List<GumballMachineRecord> findAll() throws IOException {
+        return gumballRepository.findAll();
+    }
+
+    @Override
+    public GumballMachineRecord findById(String id) throws IOException {
+        return gumballRepository.findById(id);
+    }
+
+    @Override
+    public String save(GumballMachineRecord gumballMachineRecord) throws IOException {
+        return gumballRepository.save(gumballMachineRecord);
+    }
+}
